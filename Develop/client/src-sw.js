713f5d9a16e58f84032c89ac20342944986d77eb
DomoMyles <47869842+DomoMyles@users.noreply.github.com>
2022-11-16 13:56:57 -0500

    getting started scwk + webpackconifg

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -25,6 +25,22 @@ warmStrategyCache({
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+p
+
+//is there anythuing else i need to do to register the serveice worker
+//when page loads it is going to register the serverice worker
+//service worker is registered for caching
+//added function of regester tyo route anything else i need in this js
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/src-sw.js');
+  });
+
+  this.addEventListener('fetch', function (event) {
+    // This fetch function is required for the service worker to be detected and is intentionally empty
+  });
+}
 
 // TODO: Implement asset caching
+
 registerRoute();
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
@@ -17,13 +20,55 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    ///added plugins  for operation, html, minicss, webpackmanifest, 
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
+      new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW(),
+///You can name whatever you want (file created name)
+      new GenerateSW(),
+      new WebpackPsaManifest({
+        name: 'DomoEditing',
+        short_name: 'Domed',
+        description: 'Just another text editor',
+        background_color: '654321',
+        theme_color: 'black',
+////        What are these next 2 for
+        start_url: 'Domo.com',
+        publicPath: '',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      }),      
     ],
 
     module: {
+      ///add mini css and css loader
       rules: [
-        
+        ////sdd mini css
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        ////adds the babel loader
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
